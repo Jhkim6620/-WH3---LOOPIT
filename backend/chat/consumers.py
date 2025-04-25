dc5c5338f17282608c79efcf85364f02dd0df498
tony <Jhkim6620>
2025-04-25 23:55:53 +0900

    서비스 개발 완료

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -4,9 +4,10 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_id = self.scope['url_route']['kwargs']['room_id']
-        self.room_group_name = f'chat_{self.room_id}'
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f"chat_{self.room_name}"
 
+        # 그룹에 참가
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -15,27 +16,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # 그룹 나가기
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
+    # 클라이언트로부터 메시지 수신 시
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
-        username = data.get('username', '익명')
 
+        # 그룹에 메시지 전송
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username,
             }
         )
 
+    # 그룹에서 메시지 수신
     async def chat_message(self, event):
+        message = event['message']
+
         await self.send(text_data=json.dumps({
-            'message': event['message'],
-            'username': event['username']
+            'message': message
         }))
--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,7 +1,17 @@
+# config/asgi.py
 import os
-import django
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-application = get_default_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns  # 이게 핵심!
+        )
+    ),
+})
